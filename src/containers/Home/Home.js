57d66d1ab1ed6c42bb28f43e3d6b21462fb0b669
Unknown <Calvin.lapp@dcmail.ca>
2019-02-04 15:32:13 -0500

    Antd working - css

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './Home.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
