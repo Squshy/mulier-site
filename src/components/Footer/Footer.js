5393ca4cae329351cfd9e7a2e935d5c32b8b3244
Unknown <Calvin.lapp@dcmail.ca>
2019-02-28 17:01:32 -0500

    Added Font Awesome

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Row, Col, Layout, Menu, Icon } from 'antd';
 import './Footer.less';
-import { Images } from '../../Themes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const { Footer } = Layout;
 
 class FooterContainer extends Component {
-    state = {
-
-    }
-
-
     render() {
         return (
             <Footer className="mulier-footer py-5">
@@ -20,14 +16,22 @@ class FooterContainer extends Component {
                         <div className="col-xs-12 col-s-6 col-md-4 col-lg-4">
                             <p>Mulier</p>
                             <hr/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                         </div>
                         <div className="col-xs-12 col-s-6 col-md-4 col-lg-4">
-                            <p>HEYY</p>
+                            <p>Contact</p>
                             <hr/>
+                            <p>123 Fake Street</p>
+                            <p>Toronto, ON</p>
+                            <p>289 905 2899</p>
                         </div>
                         <div className="col-xs-12 col-s-6 col-md-4 col-lg-4">
-                            <p>HEYY</p>
+                            <p>Socials</p>
                             <hr/>
+                            <a href="https://www.instagram.com/mulier.to/">
+                                <FontAwesomeIcon icon={faInstagram} size="lg"/>
+                                &nbsp;<span>Mulier.to</span>
+                            </a>
                         </div>
                     </div>
                 </div>
